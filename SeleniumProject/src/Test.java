8e7e3749bb42e3567832c3a37a7000983d77db76
Dinesh <dineshkrishna2303@gmail.com>
2020-03-17 18:00:33 +0530

    Print Even Numbers

--- SeleniumProject/src/Test.java
+++ SeleniumProject/src/Test.java
@@ -3,6 +3,12 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		System.out.println("Java Program");
+		for(int i=1;i<=20;i++) {
+			if(i%2==0) {
+				System.out.print(i+",");
+			}
+			
+		}
 	}
 
 }
